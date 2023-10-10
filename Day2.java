294f1aed9be12d1ab45989d20d50fc4231b624f2
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-10 17:46:18 +0800

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+       boolean data = true;
+       char karakter = '/';
+       byte angka = 1;
+       short angka1 = 2;
+       int angka2 = 3;
+       long angka3 = 4;
+       float angka4 = 1.1f;
+       double angka5 = 2.1;
+       
+        System.out.println(data);
+        System.out.println(karakter);
+        System.out.println(angka);
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println(angka3);
+        System.out.println(angka4);
+        System.out.println(angka5);
+    }
+    
+}

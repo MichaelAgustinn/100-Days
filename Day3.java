ca99cfaeb68bdadca5a4aef5557ae71b890713a2
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-11 15:45:36 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        
+        double tinggi_badan = 143.7;
+        int beratBadan = 34;
+        int umur = 12;
+        boolean status = false;
+        System.out.println(tinggi_badan);
+        System.out.println(beratBadan);
+        System.out.println(umur);
+        System.out.println(status);
+    }
+    
+}

9dc8ed7ca9e3eb856f37255c90cc90a8e48a7ba2
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-12 16:52:52 +0800

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class Day4 {
+
+    
+    public static void main(String[] args) {
+//        Day 4  :D
+        char jenis_kelamin = 'L';
+        String nama = "Michael Agustin";
+        
+        System.out.println("Nama\t\t: " + nama + "\n" + "Jenis Kelamin\t: " + jenis_kelamin);
+        
+    }
+    
+}

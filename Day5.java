b2e0be99efe4fc01aceef46da801257ca918e72b
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-13 19:08:32 +0800

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+//        DAY 5 😎😎
+    int hari_ke = 5 ;
+    System.out.println("Sekarang Coding Hari Ke : "+hari_ke);
+    }
+    
+}

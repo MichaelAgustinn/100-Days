8daef1b6ddbaf6bfa601a728f7929f089fc35390
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-14 09:49:22 +0800

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,16 @@
+package day6;
+public class Day6 {
+    public static void main(String[] args) {
+//      day 6 :;
+    String nama = "Michael Agustin";
+    String fakultas = "Teknik";
+    String prodi = "Informatika";
+    String nim = "D0223310";
+    short angkatan = 2023;
+    char kelas = 'A';
+    
+    System.out.println("Data Mahasiswa"+"\n"+"Nama \t\t: "+nama+"\n"+"Nim\t\t: "+nim+"\n"+"Angakatan \t: "+angkatan+"\n"+"Fakultas \t: "+fakultas+"\n"+"Prodi\t\t: "+prodi+"\n"+"Kelas \t\t: "+kelas);
+        
+    }
+    
+}

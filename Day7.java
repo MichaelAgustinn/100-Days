49612b944ce87d5c8991e633c5365e25e470b8a6
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-15 10:57:16 +0800

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,27 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Day7 {
+    public static void main(String[] args) {
+//        DAY 7 😎😎 
+        Scanner in = new Scanner (System.in);
+        String ab = in.nextLine();
+        double bb = in.nextDouble();
+        int cb = in.nextInt();
+                
+        char seven = '7';
+        
+        System.out.println(seven+" Lapisan Langit");
+        System.out.println(seven+" Warna Pelangi");
+        System.out.println(seven+ " Tangga Nada");
+        System.out.println("Lucky Number "+seven);
+        System.out.println(seven+" Deadly Sins");
+        System.out.println(seven+" Wonders of the World");
+        System.out.println(seven+" Lautan");
+        System.out.println(seven+" Benua");
+        System.out.println("Liverpool "+seven+":0 Man United");
+        
+    }
+    
+}

aa8c6e6cbe8b60469cc2bdd7f7b8a095ef63d2d8
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-26 15:55:18 +0800

    menampilkan tipe data BigInteger
    Mencetak/Menampilkan tipe data BigInteger

---
+++ Day18.java
@@ -0,0 +1,9 @@
+package day18;
+
+import java.math.BigInteger;
+public class Day18 {
+    public static void main(String[] args) {
+       BigInteger a = new BigInteger ("40572274183");
+       System.out.println (a);
+    } 
+}

66cfd25180ce1c35a3b85d868da1c41e8ecfdceb
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-27 17:39:52 +0800

    perulangan "for"
    membuat perulangan nama dengan perulangan "for" sebanyak 10 kali

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package day19;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+        for (int m = 1; m <= 10; m++){
+            System.out.println(m+". miky");
+        }
+    }
+    
+}

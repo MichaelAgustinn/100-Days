e5d3e06fd28fd7f7a7caf00920d9d02f77f822b5
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-29 20:45:00 +0800

    mengkonversi tipe data
    Mengkonversi tipe data (String) ke tipe data primitif (long)

---
+++ Day21.java
@@ -0,0 +1,12 @@
+package day21;
+
+public class Day21 {
+
+    public static void main(String[] args) {
+//        mengonversi tipe data (String) ke tipe data primitif (long)
+        String nomor = "999999";
+        long nomor1 = Long.parseLong(nomor);
+        System.out.println(nomor1+1);
+    }
+    
+}

f82e15746b4afbe804c86a095a8dc834d7522cf6
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-10-30 19:13:15 +0800

    mengkonversi tipe data String
    Mengkonversi tipe data String ke tipe float

---
+++ Day22.java
@@ -0,0 +1,9 @@
+package day22;
+
+public class day22 {
+  public static main void (String[]args){
+    String nomor = "178.7";
+    float nomor1 = Float.parseFloat(nomor);
+    System.out.println(nomor1+1);
+  }
+}

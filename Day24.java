9bea256569a31502aa995619ed849bb7c6dbbdf8
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-01 22:14:45 +0800

    Perulangan For Down To (backward)
    membuat perulangan yang hanya menampilkan angka ganjil di bawah 10

---
+++ Day24.java
@@ -0,0 +1,14 @@
+package day24;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        for (int m = 10; m >= 0; m--) {
+            if (m%2==1){
+                System.out.println(m);
+            }
+            
+        }
+    }
+    
+}

d0b0095831bd4893856f10ede58475b075cafc12
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-02 20:22:10 +0800

    Increment di perulangan For (+=2)
    mencetak perulangan Increment angka genap <= 10

---
+++ Day25.java
@@ -0,0 +1,12 @@
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        for (int p = 1; p <= 10; p += 2) {
+        System.out.println(p+1);
+        }
+        
+    }
+    
+}

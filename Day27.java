e7329e8421cac4940b33d7c37a3a3795f47ec825
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-04 21:30:57 +0800

    Perulangan While
    mencetak perulangan while tipe data integer dibawah 6 dan dimulai dari 1

---
+++ Day27.java
@@ -0,0 +1,13 @@
+package day27;
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        int angka = 1;
+        while (angka < 6) {
+            System.out.println("hitung: " + angka);
+            angka++;
+        }
+    }
+    
+}

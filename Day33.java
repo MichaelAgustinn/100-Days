e85bf3d6b6a58fd224bb982ff63f9ba75841096e
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-10 23:04:02 +0800

    Deklarasi Array
    Mendeklarasikan Array String dan mengakses elemen array

---
+++ Day33.java
@@ -0,0 +1,9 @@
+package day100coding;
+
+public class Day33 {
+            public static void main(String[] args) {
+            String[] nama = {"Miky", "Suardi", "Ivan", "Azzam",};
+            System.out.println(nama[0]);
+    }
+ 
+}

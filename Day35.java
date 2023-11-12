4edd8582c4d766b42219a310077aa9bdbdd380b1
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-12 23:11:13 +0800

    Deklarasi Array bertipe primitif
    mendeklarasikan array bertipe double

---
+++ Day35.java
@@ -0,0 +1,9 @@
+package coding.pkg100.days;
+
+public class Day35 {
+    public static void main(String[] args) {
+         double[] angkaPecahan = {1.2, 2.4, 3.6, 4.8, 5.0};
+         System.out.println(angkaPecahan[0]);
+    }
+ 
+}

467cc4ea33872708dbfcd45938d49e2ce2f3b090
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-13 18:59:19 +0800

    Deklarasi Array bertipe primitif (long)
    mendeklarasikan array bertipe long

---
+++ Day36.java
@@ -0,0 +1,10 @@
+package day100coding;
+
+public class Day36 {
+    public static void main(String[] args) {
+        long[] Long = {123, 345, 678, 999};
+            for(long angka : Long){
+                System.out.println("angka "+angka);
+            }
+    }
+}

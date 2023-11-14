26bff0d1d3112d494da047b0fa1133549502f69f
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-14 23:45:19 +0800

    Deklarasi Array bertipe non-primitif (String)
    mendeklarasikan array bertipe non-primitif (String)

---
+++ Day37.java
@@ -0,0 +1,16 @@
+package coding.pkg100.days;
+
+public class Day37 {
+    public static void main(String[] args) {
+        String[] namaHari = {"senin","selasa","rabu","kamis","jumat","sabtu","minggu"};
+
+        System.out.println("Hari pertama\t: " + namaHari[6]);
+        System.out.println("Hari kedua\t: " + namaHari[1]);
+        System.out.println("Hari ketiga\t: " + namaHari[2]);
+        System.out.println("Hari keempat\t: " + namaHari[3]);
+        System.out.println("Hari kelima\t: " + namaHari[4]);
+        System.out.println("Hari keenam\t: " + namaHari[5]);
+        System.out.println("Hari ketujuh\t: " + namaHari[0]);
+    }
+   
+}   

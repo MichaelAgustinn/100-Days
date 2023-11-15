0ef20d4e1d93bd1e750ec2620cef17f79c434ad0
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-15 22:44:24 +0800

    Array
    menampilkan semua isi array

---
+++ Day38.java
@@ -0,0 +1,11 @@
+package day100coding;
+
+public class Day38 {
+    public static void main(String[] args) {
+        String[] miky = {"miky","aril","arham","suardi","miky2"};
+        System.out.println("Isi array: ");
+        for (int i = 0; i < miky.length; i++) {
+            System.out.println(miky[i]);
+        }
+}
+}

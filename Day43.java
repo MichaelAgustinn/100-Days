8fdc9e80958b9253f9070d86459e1a4418290ca7
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-20 21:06:03 +0800

    menampilkan semua elemen array
    menampilkan semua elemen array menggunakan Arrays.toString

---
+++ Day43.java
@@ -0,0 +1,10 @@
+package day100coding;
+
+import java.util.Arrays;
+
+public class Day43 {
+    public static void main(String[] args) {
+        int[] nomor = {0,1,2,3,4,5,6,7,8,9};
+        System.out.println("isi array: "+Arrays.toString(nomor));
+    }
+}

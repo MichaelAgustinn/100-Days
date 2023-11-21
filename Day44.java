0f76ea667521525a7854d8958af88ce489339aee
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-21 22:38:30 +0800

    Tampilkan item dengan perulangan
    menampilakn nilai array dengan perulangan

---
+++ Day44.java
@@ -0,0 +1,12 @@
+package coding.pkg100.days;
+
+public class Day44 {
+    public static void main(String[] args) {
+        
+        String[] array = {"test","test2","test3"};
+        
+        for (String value : array) {
+            System.out.println(value);
+        }
+}
+}

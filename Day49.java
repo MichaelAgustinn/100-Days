8726752d06acbe242a96f602ad50d6491d7dde2a
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-26 23:45:56 +0800

    Penggunaan class Arrays untuk menampilkan semua elemen array

---
+++ Day49.java
@@ -0,0 +1,8 @@
+package coding.pkg100.days;
+import java.util.Arrays;
+public class Day49 {
+    public static void main(String[] args) {
+    int[] nomor = {1, 2, 3, 4, 5};
+    System.out.println("array nomor: "+Arrays.toString(nomor));
+    }
+}

44b0755fecc533645a53e6a0fc5392f90b0ca6ae
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-11-30 19:18:06 +0800

    Tampilkan item dengan perulangan
    menampilakan isi array dengan perulangan for(String .. : .. )

---
+++ Day53.java
@@ -0,0 +1,11 @@
+package coding.pkg100.days;
+
+public class Day53 {
+    public static void main(String[] args) {
+        String[] buah = {"apel", "nanas", "anggur"};
+        
+        for (String fruit : buah) {
+            System.out.println(fruit);
+        }
+    }
+}

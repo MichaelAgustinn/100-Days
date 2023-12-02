5291600f0b84c06d667ef45a16afd699a83fc8e4
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-02 23:54:43 +0800

    menghitung keliling lingkaran(method/function)
    menghitung keliling lingkaran menggunakan pemanggilan method/function

---
+++ Day55.java
@@ -0,0 +1,21 @@
+package Day55;
+
+public class main{
+
+public static double luasLingkaran(double PI, double jariJari){
+    return (PI*jariJari*jariJari);
+    }
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner (System.in);
+        double hasil;
+        
+        System.out.print("masukkan jari-jari: ");
+        double jariJari = in.nextDouble();
+        
+        hasil = luasLingkaran(3.14,jariJari);
+        
+        System.out.println("Hasil Luas Lingkaran = "+hasil);
+        
+        }
+}

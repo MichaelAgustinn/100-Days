b1fd7ce6921818df84206d3799aaf13a861ff996
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-04 22:37:57 +0800

    Fungsi void
    Membuat function dan di panggil di main

---
+++ Day57.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Main {
+
+    public static void test(){
+        System.out.println("Miky");
+    }
+    
+    public static void main(String[] args){
+        test();
+        }
+    }

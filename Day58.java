cd7c23a6f9bc1a52d51c6066272c0e7d41d795c0
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-05 23:29:34 +0800

    Method Void
    membuat out print di method dan di panggil di main

---
+++ Day58.java
@@ -0,0 +1,10 @@
+package day100coding;
+
+public class Day58 {
+    public static void miky(){
+        System.out.println("Nama Saya Miky");
+    }
+    public static void main(String[] args) {
+        miky();
+    }
+}

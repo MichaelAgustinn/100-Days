25ee7b97e0807c974dd0a51beb1b227a3f48c922
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-06 22:37:53 +0800

    .toLowerCase
    merubah String nama menjadi huruf kecil menggunakan .toLowerCase

---
+++ Day59.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class Main { 
+    public static void main(String[] args){
+        String nama = "mIkY";
+        String nama1 = nama.toLowerCase();
+        System.out.println(nama1);
+        }
+    }

cd69f2a43817cf62e6c5906cf587a530deb40838
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-16 23:07:44 +0800

    String, StringBuffer, StringBuilder String charAt( )

---
+++ Day69.java
@@ -0,0 +1,10 @@
+public class test {
+public static void main(String[] args){
+        String str = "Hello, World!";
+    
+        char chr = str.charAt(2);
+        
+        System.out.println("karakter di index 2: " + chr);
+        
+}
+}

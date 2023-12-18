1e23b7017b7997c8b13d097fc6ffcd1f2a8fda20
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-18 22:56:57 +0800

    membuat segiempat dari perulangan

---
+++ Day71.java
@@ -0,0 +1,11 @@
+public class test {
+public static void main(String[] args){
+    
+    for(int i = 0; i <= 6; i++){       //luping kebawah
+            for(int a = 0; a <= 6; a++){   //luping ke kanan
+                System.out.print("* ");
+            }
+                System.out.println("");
+        }
+}
+}

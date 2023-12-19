58c9df7fab222ff6078a74b873be7c5bed1849e7
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-19 22:35:34 +0800

    toLowerCase()

---
+++ Day72.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+    String original = "MiChAel AgUstiN";
+String lowerCase = original.toLowerCase();
+System.out.println(lowerCase);
+    }
+}

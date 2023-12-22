9388fae0b5fd37d547abdf438083522b27e0c06f
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-22 18:54:31 +0800

    Math.ceil

---
+++ Day75.java
@@ -0,0 +1,11 @@
+package day100coding;
+
+public class Day75 {
+    public static void main(String[] args) {
+        
+        double bulan = 55000000.0/1500000;
+        double done = Math.ceil(bulan);
+        
+        System.out.println("hasil= "+done);
+    }
+}

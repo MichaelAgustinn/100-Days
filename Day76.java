0e9b9cf9cf80cd8868e0159c2f945d9ccfc16ab4
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-23 12:06:28 +0800

    Menyelesaikan tugas
    Jika menginput bilangan ganjil, program berhenti

---
+++ Day76.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        while(true){
+            System.out.print("masukkan angka: ");
+        int angka = in.nextInt();
+            
+            if(angka%2==0){
+                break;
+        }
+        
+    }
+}
+}

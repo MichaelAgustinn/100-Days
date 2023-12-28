693cd8ff7df58adc561dd3cdee44ea98a81b4b7c
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2023-12-28 08:47:38 +0800

    String charAt()

---
+++ Day82.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class testjava {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String kata = "yuhu";
+
+        char up = kata.charAt(4);
+
+        System.out.println("index ke 4 dari kata = " + up);
+
+        in.close();
+    }
+}

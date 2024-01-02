a04b178ab952636643013813d08132b268902f31
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2024-01-02 22:05:27 +0800

    .deleteCharAt
    stringBuilder & deleteCharAt

---
+++ Day86.java
@@ -0,0 +1,10 @@
+public class day86 {
+    
+    public static void main(String[]args){
+        
+        StringBuilder nama = new StringBuilder("mikyy");
+        nama.deleteCharAt(3);
+        System.out.println(nama);
+        
+    }
+}

5736ab975896d2dd22620fabb49df032fdb3c10b
MichaelAgustinn <147056587+MichaelAgustinn@users.noreply.github.com>
2024-01-03 21:39:16 +0800

    .deleteCharAt.2
    StringBuffer & deleteCharAt

---
+++ Day87.java
@@ -0,0 +1,10 @@
+public class day87 {
+
+    public static void main(String[] args) {
+
+        StringBuffer nama = new StringBuffer("michael");
+        nama.deleteCharAt(5);
+        System.out.println(nama);
+
+    }
+}
